1db2040171a4c740478bf99b56aa1fc9d587b773
GloriaAndonyaIT <gloriaandonyaa@gmail.com>
2025-02-28 21:44:47 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    const input = document.getElementById("button");
+  
+    if (input) {
+      input.addEventListener("click", function() {
+        alert("I was clicked!");
+      });
+    }
+  }
+  
+  addingEventListener();
+  
+  
